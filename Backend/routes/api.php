55293545785efaab88f1ad4194e180bcfd4d684f
Arjuna Lanang <wardilanang46@gmail.com>
2026-03-14 16:08:45 +0700

    feat: Add API routes for user registration, login, logout, and user information.

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -4,12 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class , 'register']);
+Route::post('/login', [AuthController::class , 'login']);
+Route::get('/', function () {
+    return response()->json([
+    'message' => 'Welcome to SMA 1 Pamekasan API',
+    ]);
+});
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class , 'logout']);
     Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-});
+            return $request->user();
+        }
+        );    });
